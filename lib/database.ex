9eb8137afd214e829edc31bf533b94b4b330fb38
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-02-24 17:54:44 +0900

    Add worker pooling
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- lib/database.ex
+++ lib/database.ex
@@ -3,30 +3,35 @@ defmodule Todo.Database do
 
   def start(db_folder), do: GenServer.start(__MODULE__, db_folder, name: :database_server) 
 
-  def store(key, data), do: GenServer.cast(:database_server, {:store, key, data})
+  def store(key, data) do
+    key
+    |> get_worker()
+    |> Todo.DatabaseWorker.store(key, data)
+  end
 
-  def get(key), do: GenServer.call(:database_server, {:get, key})
+  def get(key) do
+    key
+    |> get_worker()
+    |> Todo.DatabaseWorker.get(key)
+  end
   
   def init(db_folder) do
-    File.mkdir_p(db_folder)
-    {:ok, db_folder}
+    {:ok, start_workers(db_folder)}
   end
 
-  def handle_cast({:store, key, data}, db_folder) do
-    file_name(db_folder, key)
-    |> File.write!(:erlang.term_to_binary(data))
-
-    {:noreply, db_folder}
-  end
-
-  def handle_call({:get, key}, _, db_folder) do
-    data = case File.read(file_name(db_folder, key)) do
-      {:ok, contents} -> :erlang.binary_to_term(contents)
-      _ -> nil
+  defp start_workers(db_folder) do
+    for index <- 0..2, into: Map.new() do
+      {:ok, worker_pid} = Todo.DatabaseWorker.start(db_folder)
+      {index, worker_pid}
     end
-
-    {:reply, data, db_folder}
   end
 
-  defp file_name(db_folder, key), do: "#{db_folder}/#{key}"
+  def get_worker(key) do
+    GenServer.call(:database_server, {:get_worker, key})
+  end
+
+  def handle_call({:get_worker, key}, _, workers) do
+    worker_key = :erlang.phash2(key, 3)
+    {:reply, Map.get(workers, worker_key), workers}
+  end
 end
